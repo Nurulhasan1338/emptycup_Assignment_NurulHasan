1b1f48590f0cc6b3d92587c58407b257c43ba2ef
Nurul Hasan <2020kucp1109@iiitkota.ac.com>
2023-11-23 20:02:01 +0530

    for testing3

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const PORT = 7000;
-// const connectToMongo = require('./db');
+const connectToMongo = require('./db');
 const cors = require('cors')
 const express = require('express')
-// const records = require("./module/recorde.js");
+const records = require("./module/recorde.js");
 const app = express()
-// connectToMongo();  
+connectToMongo();  
 
 app.use(cors(
   {
-    origin:[""],
+    origin:["https://emptycup-backend-nurul.vercel.app/"],
     methods:["GET"],
     credentials:true
   }
@@ -16,16 +16,16 @@ app.use(cors(
 app.use(express.json());
 
 
-app.get('*',async(req,res)=>{
+app.get('/api',async(req,res)=>{
     try {
   
-        // const data = await records.find();
-        // if(data.length!==0){
-        //  res.json({"success":true,"data":data});
-        // }
-        // else{
-        //  res.json({"success":false,"msg":"no problem"});
-        // }
+        const data = await records.find();
+        if(data.length!==0){
+          return res.status(200).json({"success":true,"data":data});
+        }
+        else{
+         res.json({"success":false,"data":[]});
+        }
         return res.status(200).json({"success":true,"data":"aa gya kya"});
        }  catch (error) {
            console.error(error);
@@ -36,38 +36,30 @@ app.get('*',async(req,res)=>{
 
 
 
-// app.put('/api/update/:id',async(req,res)=>{
-//     try {
+app.put('/api/update/:id',async(req,res)=>{
+    try {
      
-//       newRecord = {
-//         name : req.body.name,
-//         shortlisted : req.body.shortlist
-//       }
+      newRecord = {
+        name : req.body.name,
+        shortlisted : req.body.shortlist
+      }
 
-//       let rec = await records.findById(req.params.id);
+      let rec = await records.findById(req.params.id);
 
-//       if(!rec) {return res.status(404).send("Not Found <3");}
+      if(!rec) {return res.status(404).send("Not Found <3");}
 
-//       console.log(newRecord)
+      console.log(newRecord)
 
-//     const note = await records.findByIdAndUpdate(req.params.id,{$set:newRecord},{new:true})
-//     res.json(note);
+    const note = await records.findByIdAndUpdate(req.params.id,{$set:newRecord},{new:true})
+    res.json(note);
 
-//        }  catch (error) {
-//            console.error(error);
-//            res.status(500).send("some internal error");
-//        }
+       }  catch (error) {
+           console.error(error);
+           res.status(500).send("some internal error");
+       }
 
-// })
+})
 
-
-// app.get('*',(req,res)=>{
-//   res.status(200).json({
-//     message:'connected successfully'
-//   })
-
-  
-// })
 
 app.listen(PORT,()=>{
   console.log(`sever is listening at :https://localhost:${PORT}`);
